df4100f0bddb2e221de9f7696d3fd70489c482e6
Trishant <trishantjain4444@gmail.com>
2023-03-28 02:16:07 +0530

    Added /createuser api logic to add signup data in database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router()
-const User =  require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.get('/createuser', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    console.log('saved in database');
-    res.send(req.body);
+
+router.post('/createuser', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('name', 'Enter a valid name').isLength({ min: 6 }),
+    body('phone', 'Enter a valid phone').isNumeric({ min: 10 }),
+    body('password', 'set password of atleast 5 character').isLength({ min: 5 })
+], async (req, res) => {
+    console.log('entered in creating loop');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        
+    }
+
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry! User with email already exists" })
+        }
+        console.log('checking email');
+
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone, 
+            password: req.body.password
+        });
+        console.log('user created');
+
+    } catch {
+        console.error(error.message)
+        res.status(500).send("Some error occured")
+
+    }
 })
 
 module.exports = router
