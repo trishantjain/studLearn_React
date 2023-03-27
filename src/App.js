d1b8436829e63daca7b8261255098b81ae9b7009
Trishant <trishantjain4444@gmail.com>
2023-03-28 02:14:11 +0530

    Changing Login component name to Signup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes }
 import Index from './components/Index';
 import About from './components/About';
 import Question from './components/Question';
-import Login from './components/Login';
+import Signup from './components/Signup';
 import Contact from './components/Contact';
 
 function App(props) {
@@ -16,7 +16,7 @@ function App(props) {
           <Route exact path="/" element={<Index />} ></Route>
           <Route exact path="/about" element={<About />} ></Route>
           <Route exact path="/question" element={<Question />} ></Route>
-          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/signup" element={<Signup />}></Route>
           <Route exact path="/contact" element={<Contact />}></Route>
         </Routes>
       </Router>
