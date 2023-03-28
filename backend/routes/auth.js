c6fb4dccddc427e361d3fc0ed6d992e207e329c1
Trishant <trishantjain4444@gmail.com>
2023-03-28 22:54:39 +0530

    Formatting auth.js in backend route folder

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,14 +7,14 @@ const { body, validationResult } = require('express-validator');
 router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('name', 'Enter a valid name').isLength({ min: 6 }),
-    body('phone', 'Enter a valid phone').isLength({ min: 10, max:10 }),
+    body('phone', 'Enter a valid phone').isLength({ min: 10, max: 10 }),
     body('password', 'set password of atleast 5 character').isLength({ min: 5 })
 ], async (req, res) => {
     console.log('entered in creating loop');
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
-        
+
     }
 
     try {
@@ -27,7 +27,7 @@ router.post('/createuser', [
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            phone: req.body.phone, 
+            phone: req.body.phone,
             password: req.body.password
         });
         console.log('user created');
