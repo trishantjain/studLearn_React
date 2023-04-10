61104925b775471bf0ac99176b2753fdf7639bb1
trishantjain <trishantjain8888@gmail.com>
2023-04-11 00:46:28 +0530

    Added env variable instead of login API in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ function Login(props) {
     // Function send the auth token to login to user
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/auth/login`, {
+        const response = await fetch(process.env.REACT_APP_LOGIN_API, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
