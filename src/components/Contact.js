bdef307fff93ac7423c86b139e1883e3a369e974
trishantjain <trishantjain8888@gmail.com>
2023-07-23 01:36:30 +0530

    Added Frontend Part of Contact Page in App

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,118 @@
 import React from 'react'
 import Navbar from './Navbar'
+import './css/contact.css'
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import { AccountCircle, Email, Phone } from '@mui/icons-material';
+import { InputAdornment } from '@mui/material'
+import Textarea from '@mui/joy/Textarea';
 
 function Contact() {
     return (
-        <div>
+        <>
             <Navbar style={{ backgroundColor: "#041858" }} active6={"active"} />
-            This is contact Page.
-        </div>
+            <div className="container-xxl py-5" id="contact">
+                <div className="container py-5 px-lg-5">
+                    <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+                        <h5 className="text-primary-gradient fw-medium">Contact Us</h5>
+                        <h1 className="mb-5 font-link">Get In Touch!</h1>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-lg-9">
+                            <div className="wow fadeInUp" data-wow-delay="0.3s">
+                                <Box className="row g-3 user-input-wrp">
+                                    <div className="form-floating col-12">
+                                        <div className="mb-4">
+                                            <TextField
+                                                required
+                                                fullWidth
+                                                id="name"
+                                                label="Your Name"
+                                                name="name"
+                                                className='form-control col-md-6 name mb-4'
+                                                autoComplete="name"
+                                                size='small'
+                                                variant='filled'
+                                                autoFocus
+                                                sx={{
+                                                    paddingRight: "10px"
+                                                }}
+                                                InputProps={{
+                                                    startAdornment: (
+                                                        <InputAdornment position='start'>
+                                                            <AccountCircle
+                                                                fontSize='small'
+                                                            />
+                                                        </InputAdornment>
+                                                    )
+                                                }}
+                                            />
+                                            <TextField
+                                                required
+                                                fullWidth
+                                                id="phone"
+                                                label="Your Phone"
+                                                name="phone"
+                                                className='form-control col-md-6'
+                                                autoComplete="phone"
+                                                size='small'
+                                                variant='filled'
+                                                autoFocus
+                                                InputProps={{
+                                                    startAdornment: (
+                                                        <InputAdornment position='start'>
+                                                            <Phone
+                                                                fontSize='small'
+                                                            />
+                                                        </InputAdornment>
+                                                    )
+                                                }}
+                                            />
+                                        </div>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="email"
+                                            label="Your Email"
+                                            name="email"
+                                            className='form-control col-12 mb-5'
+                                            autoComplete="email"
+                                            size='small'
+                                            variant='filled'
+                                            autoFocus
+                                            InputProps={{
+                                                startAdornment: (
+                                                    <InputAdornment position='start'>
+                                                        <Email
+                                                            fontSize='small'
+                                                        />
+                                                    </InputAdornment>
+                                                )
+                                            }}
+                                        />
+                                    </div>
+                                    <Textarea
+                                        className="form-floating col-md-12 mb-5"
+                                        placeholder="Type..."
+                                        minRows={6}
+                                        variant='soft'
+                                        sx={{
+                                            margin: "15px"
+                                        }}
+
+                                    />
+                                    <div className="col-12 text-center">
+                                        <button className="btn btn-primary-gradient rounded-pill py-3 px-5" type="submit">Send Message</button>
+                                    </div>
+                                </Box>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
-}
+
+};
 
 export default Contact
